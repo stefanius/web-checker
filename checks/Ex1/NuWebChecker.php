c208ed9386e43f14873a8c3e1378554ace533e1e
stefanius <sgrootveld@gmail.com>
2015-12-21 23:37:14 +0100

    Implement @dataProvider annotation.

--- checks/Ex1/NuWebChecker.php
+++ checks/Ex1/NuWebChecker.php
@@ -1,5 +1,7 @@
 <?php
 
+use Stefanius\WebChecker\Annotations\DataProvider;
+
 class NuWebChecker extends Stefanius\WebChecker\Checker\WebCheck
 {
     public function checkDvdw()
@@ -20,5 +22,22 @@ class NuWebChecker extends Stefanius\WebChecker\Checker\WebCheck
             ->see('Zaterdag 12 december 2015')
             ->mustContainH1('195 landen stemmen in met wereldwijd klimaatverdrag Parijs')
             ->descriptionIs('Het laatste nieuws het eerst op NU.nl');
+    }
+
+    /**
+     * @dataProvider henk
+     */
+    public function checkWithDataProvider($p1, $p2, $p3)
+    {
+
+    }
+
+    public function henk()
+    {
+        return [
+            ['1', '2', '3'],
+            ['uno', 'dos', 'dres'],
+            ['aap', 'noot', 'mies'],
+        ];
     }
 }
