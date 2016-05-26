c285478760d6ee24c9f176ccfb7d5bcecd5ae653
Stefanius <sgrootveld@gmail.com>
2016-05-26 22:59:34 +0200

    Comment out not implemented functions.

--- checks/Ex1/NuWebChecker.php
+++ checks/Ex1/NuWebChecker.php
@@ -5,8 +5,8 @@ class NuWebChecker extends Stefanius\WebChecker\Checker\WebCheck
     public function checkDvdw()
     {
         $this->visit('dagvandeweek.nl')
-            ->assertResponseOk()
-            ->assertResponseStatus(200)
+          //  ->assertResponseOk()
+           // ->assertResponseStatus(200)
             ->pageTitleIs('Alle dagen van de week welkom bij DagVanDeWeek.nl')
             ->hasRobotsIndex();
     }
@@ -14,8 +14,8 @@ class NuWebChecker extends Stefanius\WebChecker\Checker\WebCheck
     public function checkNu()
     {
         $this->visit('http://www.nu.nl')
-            ->assertResponseOk()
-            ->assertResponseStatus(200)
+           // ->assertResponseOk()
+            //->assertResponseStatus(200)
             ->see('Zaterdag 12 december 2015')
             ->mustContainH1('195 landen stemmen in met wereldwijd klimaatverdrag Parijs')
             ->descriptionIs('Het laatste nieuws het eerst op NU.nl');
